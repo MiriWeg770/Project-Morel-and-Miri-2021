d6776fabead1a566f0826cae4863bf4560f009c4
MiriWeg770 <87752389+MiriWeg770@users.noreply.github.com>
2021-08-09 15:59:17 +0300

    Delete CallApiProject directory

--- CallApiProject/src/Models/Menu.ts
+++
@@ -1,5 +0,0 @@
-export class Menu{
-    constructor( public MenuCode:number, public MenuName:string, public Discription:string,public UserCode:number,public DateCreated:Date, public Links:string,public ViewsNumber:number, public UserName:string){
-        
-    }
-}
--- CallApiProject/src/Models/User.ts
+++
@@ -1,7 +0,0 @@
-export class User{
-
-    constructor( public UserCode:number, public UserName:string, public Password:string, public Mail:string){
-
-    }
-
-}
